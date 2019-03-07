d490aaf0bbe19706516ac97cb017d5e140356158
Patrick Schadler <pschadler@pmone.com>
2019-03-07 08:52:45 +0100

    Update AccountController.cs

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -26,7 +26,7 @@ namespace Auth0.Controllers
                 // Indicate here where Auth0 should redirect the user after a logout.
                 // Note that the resulting absolute Uri must be whitelisted in the
                 // **Allowed Logout URLs** settings for the app.
-                RedirectUri = Url.Action("Index", "Home")
+                RedirectUri = "https://patrickschadler.com"
             });
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
         }
